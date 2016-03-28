a796d80e3bdb1ebfa94e9e81fb0a5970785261ec
VLauterbach <vlauterbach@independentsoftware.com>
2016-03-28 12:30:18 -0400

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ import main.views as views
 
 urlpatterns = [
     # in real service, url is <root_url>/users/<DN>.json
-    #url(r'^users/(?P<dn>[0-9a-zA-Z_=,\.@ ]+)/info.json/$', views.UserDnView),
+    url(r'^users/(?P<dn>[0-9a-zA-Z_=,\.@ ]+)/info.json/$', views.UserDnView),
     # in real service, url is <root_url>/users/<DN>/groups/<PROJECT>.json
     url(r'^users/(?P<dn>[0-9a-zA-Z_,=\.@ ]+)/groups/(?P<project>[a-zA-Z0-9_]+)/$', views.UserGroupsView),
 
